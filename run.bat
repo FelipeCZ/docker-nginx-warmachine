61f0e3143c1980db7cb0a2a3e1065218ef9bc17d
felipecz <flp313@gmail.com>
2018-04-24 23:46:40 -0300

    adding support scripts to help the application build and start locally

---
+++ run.bat
@@ -0,0 +1,6 @@
+echo "Building application...."
+docker-compose build 
+echo "Build completed."
+echo "Running application..."
+docker-compose up
+echo "Application exited."
---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Building application...."
+docker-compose build 
+echo "Build completed."
+echo "Running application..."
+docker-compose up
+echo "Application exited."
